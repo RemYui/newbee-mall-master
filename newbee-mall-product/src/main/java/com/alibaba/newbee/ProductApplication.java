ff2f80af9e705cbef96ce11b0d7b599fd883d3a1
RemYui <Smy99999@qq.com>
2022-03-09 16:43:29 +0800

    Default Changelist

---
+++ newbee-mall-product/src/main/java/com/alibaba/newbee/ProductApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.newbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Emilia
+ * @Since: 2022.03.09 16:40
+ */
+@SpringBootApplication
+public class ProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductApplication.class, args);
+    }
+}
