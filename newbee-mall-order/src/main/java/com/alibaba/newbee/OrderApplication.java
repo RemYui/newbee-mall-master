1e1ee7c246892151fefd5cb30f4800712f263bd0
RemYui <Smy99999@qq.com>
2022-03-09 16:55:08 +0800

    Default Changelist

---
+++ newbee-mall-order/src/main/java/com/alibaba/newbee/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.alibaba.newbee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: Emilia
+ * @Since: 2022.03.09 16:54
+ */
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+}
